50818bc5f1f05b584a1b5e875c948a96d3d5871c
John Doe <sawam2007@gmail.com>
2026-09-02 20:18:32 +0300

    theerd

---
+++ progect/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+
+def good (request):
+    return HttpResponse("<h1>ГУГУГАГА</h1>")
+
+
+def index(request):
+    return render(request, 'main/index.html')
