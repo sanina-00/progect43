a4642f3a31e113f37b5ad107004ba24bc21d4ca5
John Doe <sawam2007@gmail.com>
2026-09-08 17:33:20 +0300

    7

--- progect/main/views.py
+++ progect/main/views.py
@@ -9,7 +9,16 @@ def good (request):
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!!',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMV',
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
