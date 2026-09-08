fcce9de47dd4e3aeedc155acd7f09502f5d0604e
John Doe <sawam2007@gmail.com>
2026-09-08 18:34:27 +0300

    8

---
+++ progect/news/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50)  # название , ограничение по количеству символов (необяз.), можно поставить defolt = значение 
+    anons = models.CharField('Анонс', max_length=250)
+    full_text = models.TextField('Статья' )
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title   
